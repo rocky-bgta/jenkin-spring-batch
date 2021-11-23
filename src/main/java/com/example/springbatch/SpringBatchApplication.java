90650b1019fc3f65cd20f1f1e033cab5d7d59387
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2021-11-23 17:31:52 +0600

    POC done

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,41 +1,13 @@
 package com.example.springbatch;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Base64;
 
 @SpringBootApplication
 public class SpringBatchApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(SpringBatchApplication.class, args);
-//    }
-
-    public static void main(String[] args) throws IOException {
-        TriggerJenkinsJob();
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
     }
 
-    private static void TriggerJenkinsJob() throws IOException {
-        URL url = new URL("http://localhost:8080/job/spring-batch/build?token=11d062d7023e11765b4d8ee867b67904f9"); // Jenkins URL localhost:8080, job named 'test'
-        String user = "salahin";
-        String pass = "11d062d7023e11765b4d8ee867b67904f9"; // password or API token
-        String authStr = user + ":" + pass;
-        String basicAuth = "Basic "+ Base64.getEncoder().encodeToString(authStr.getBytes("utf-8"));
-
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty ("Authorization", basicAuth);
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:27.0) Gecko/20100101 Firefox/27.0.2 Waterfox/27.0");
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-        System.out.println("Step Tiger Jenkins Jobs: " + connection.getResponseCode());
-        connection.disconnect();
-    }
 }
--- src/main/java/com/example/springbatch/configuration/JobConfiguration.java
+++ src/main/java/com/example/springbatch/configuration/JobConfiguration.java
@@ -9,8 +9,10 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 
 @Configuration
 @EnableBatchProcessing
@@ -23,7 +25,7 @@ public class JobConfiguration {
     private static final String remoteUrl;
 
     static {
-        remoteUrl = "http://localhost:8080/job/spring-batch/build?token=123456";
+        remoteUrl = "http://localhost:8080/job/spring-batch/build?token=";
     }
 
     public JobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
@@ -53,10 +55,7 @@ public class JobConfiguration {
     public Step step3(){
         return stepBuilderFactory.get("step3")
                 .tasklet((stepContribution, chunkContext) -> {
-                    URL url = new URL(remoteUrl);
-                    HttpURLConnection con  = (HttpURLConnection) url.openConnection();
-                    System.out.println("Step Tiger Jenkins Jobs: " + con.getResponseCode());
-                    con.disconnect();
+                    triggerJenkinsJob("test-batch");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
@@ -68,5 +67,24 @@ public class JobConfiguration {
                 .next(step2())
                 .next(step3())
                 .build();
+    }
+
+
+    private static void triggerJenkinsJob(String token) throws IOException {
+        URL url = new URL(remoteUrl+token); // Jenkins URL localhost:8080, job named 'test'
+        String user = "salahin";
+        String pass = "11d062d7023e11765b4d8ee867b67904f9"; // password or API token
+        String authStr = user + ":" + pass;
+        String basicAuth = "Basic "+ Base64.getEncoder().encodeToString(authStr.getBytes("utf-8"));
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty ("Authorization", basicAuth);
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:27.0) Gecko/20100101 Firefox/27.0.2 Waterfox/27.0");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        System.out.println("Step Tiger Jenkins Jobs: " + connection.getResponseCode());
+        connection.disconnect();
     }
 }
